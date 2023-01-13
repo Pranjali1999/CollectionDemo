3cc581989b6ea9fc4df91a3b700b6e8374aee6af
Pranjali1999 <pranjaliajmire99@gmail.com>
2023-01-13 14:15:25 +0530

    Added file for arraylist

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		list.add("Pune");
+		list.add("Mumbai");
+		list.add("Bangalore");
+		list.add("Delhi");
+		System.out.println(list);
+	}
+
+}
